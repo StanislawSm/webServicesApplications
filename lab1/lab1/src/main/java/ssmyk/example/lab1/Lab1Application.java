fdfd167a67a5354f2b1b2925b6a519281a1aefd6
StanislawSm <105853049+StanislawSm@users.noreply.github.com>
2022-10-18 10:30:13 +0200

    small refactoring

--- lab1/lab1/src/main/java/ssmyk/example/lab1/Lab1Application.java
+++ lab1/lab1/src/main/java/ssmyk/example/lab1/Lab1Application.java
@@ -3,17 +3,10 @@ package ssmyk.example.lab1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Application main class.
- */
+
 @SpringBootApplication
 public class Lab1Application {
 
-    /**
-     * Application main entry point. Starts Spring Boot application context.
-     *
-     * @param args command line arguments
-     */
     public static void main(String[] args) {
         SpringApplication.run(Lab1Application.class, args);
     }
