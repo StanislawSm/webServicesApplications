89b2a32ce18ebfe4750b03a854caae17817a56d8
StanislawSm <105853049+StanislawSm@users.noreply.github.com>
2022-11-19 13:19:56 +0100

    initializing project

---
+++ lab3/usersModule/src/test/java/lab3/usersmodule/UsersModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package lab3.usersmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
